763f6a02118efa3d860293d24959aec4c446ea2d
Arthur Alexi <90854173+ArthurAlexi@users.noreply.github.com>
2023-09-24 17:40:56 -0300

    feat: add checkbox component

---
+++ Codigo/wtdt/lib/components/checkBoxWeek/CheckBoxWeek.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+import 'package:wtdt/components/checkBoxWeek/CheckBoxDay.dart';
+
+class CheckboxWeek extends StatefulWidget {
+  const CheckboxWeek({super.key});
+
+  @override
+  State<CheckboxWeek> createState() => _CheckboxWeekState();
+}
+
+class _CheckboxWeekState extends State<CheckboxWeek> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    
+
+    return const Center(
+      
+      child:  Column(
+        children: [
+          CheckboxDay(label: "Domingo"),
+          CheckboxDay(label: "Segunda-feira"),
+          CheckboxDay(label: "Terça-Feira"),
+          CheckboxDay(label: "Quarta-Feira"),
+          CheckboxDay(label: "Quinta-feira"),
+          CheckboxDay(label: "Sexta-feira"),
+          CheckboxDay(label: "Sábado"),
+        ],
+      ),
+    );
+  }
+}
