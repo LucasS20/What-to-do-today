821a3b5cde2cfa99962e03f073a11e1c8e37f433
Arthur Alexi <90854173+ArthurAlexi@users.noreply.github.com>
2023-09-25 07:26:28 -0300

    style: change the wrapper

--- Codigo/wtdt/lib/components/Header.dart
+++ Codigo/wtdt/lib/components/Header.dart
@@ -10,28 +10,26 @@ class Header extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
-    return  Center(
-      child:  Column(
-        children: [
-        const SizedBox(
-          height: 20,
+    return  Column(
+      children: [
+      const SizedBox(
+        height: 20,
+      ),
+      Text(firstPart,
+        style: const TextStyle(
+          fontSize: 50,
+          fontWeight: FontWeight.bold,
+          color: Colors.black45
         ),
-        Text(firstPart,
-          style: const TextStyle(
-            fontSize: 50,
-            fontWeight: FontWeight.bold,
-            color: Colors.black45
-          ),
+      ),
+      Text(secondtPart,
+        style: const TextStyle(
+          fontSize: 60,
+          fontWeight: FontWeight.bold,
+          color: Colors.brown
         ),
-        Text(secondtPart,
-          style: const TextStyle(
-            fontSize: 60,
-            fontWeight: FontWeight.bold,
-            color: Colors.brown
-          ),
-        ),
-        ],
-      )
+      ),
+      ],
     );
   }
 }
