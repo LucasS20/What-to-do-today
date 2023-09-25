29fa0ef8eaa97c584b3d10ba5301a7044c33822d
Pablo Magalhães <109962972+pabloaugustocm17@users.noreply.github.com>
2023-09-25 10:36:39 -0300

    feat: create text custom
    - criado texto customizado para estilo bold e italic

---
+++ Codigo/wtdt/lib/components/CustomText.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:wtdt/styles/TextStyles.dart';
+
+class CustomText extends StatelessWidget{
+  
+  const CustomText({
+    super.key,
+    required this.textLabel,
+    required this.size
+  });
+
+  final String textLabel;
+  final double size;
+  
+  @override
+  Widget build(BuildContext context) {
+    
+    TextStyles styles = TextStyles();
+
+    return Text(
+      textLabel,
+      style: styles.getTextStyleBoldItalic(size),
+    );
+
+  }
+
+
+}
