e465d285000aeb3173e60edfe07d1e95d574d469
Arthur Alexi <90854173+ArthurAlexi@users.noreply.github.com>
2023-09-26 12:59:40 -0300

    feat: add void fuction how attribute

--- Codigo/wtdt/lib/components/CustomButtom.dart
+++ Codigo/wtdt/lib/components/CustomButtom.dart
@@ -6,38 +6,43 @@ class CustomButtom extends StatefulWidget{
   const CustomButtom({
     super.key,
     required this.textLabel,
-    required this.primaryColor
+    required this.primaryColor,
+    required this.onPressed
   });
 
   final String textLabel;
   final Color primaryColor;
+  final void Function() onPressed;
 
   @override
-  State<CustomButtom> createState() => _CustomButtomState(textLabel: textLabel, primaryColor: primaryColor);
+  // ignore: no_logic_in_create_state
+  State<CustomButtom> createState() => _CustomButtomState(textLabel: textLabel, primaryColor: primaryColor, onPressed: onPressed);
 }
 
 class _CustomButtomState extends State<CustomButtom>{
 
   _CustomButtomState({
     required this.textLabel,
-    required this.primaryColor
+    required this.primaryColor,
+    required this.onPressed
   });
 
   final String textLabel;
   final Color primaryColor;
+  final void Function() onPressed;
 
   //final GlobalKey<ScaffoldState> _formKey = GlobalKey<ScaffoldState>();
   
   @override
   Widget build(BuildContext context) {
     
-    const Size min = Size(325.0, 100.0);
-    const Size max = Size(475.0, 100.0);
+    const Size min = Size(325.0, 80.0);
+    const Size max = Size(475.0, 80.0);
 
     TextStyles styles = TextStyles();
 
     return ElevatedButton(
-      onPressed: () {}, 
+      onPressed: onPressed, 
       style: ElevatedButton.styleFrom(
         backgroundColor: primaryColor,
         minimumSize: min,
@@ -48,7 +53,11 @@ class _CustomButtomState extends State<CustomButtom>{
         )
       ),
       child: Text(
-        textLabel
+        textLabel,
+        style: const TextStyle(
+          fontSize: 36,
+          fontWeight: FontWeight.w700,          
+        ),
       ),
     );
 
