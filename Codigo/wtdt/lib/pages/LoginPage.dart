da46760252c17a8464ddff39e8a7e9966275febe
Arthur Alexi <90854173+ArthurAlexi@users.noreply.github.com>
2023-10-29 18:08:03 -0300

    feat: add navigation to SingUpUser

--- Codigo/wtdt/lib/pages/LoginPage.dart
+++ Codigo/wtdt/lib/pages/LoginPage.dart
@@ -1,71 +1,85 @@
+// ignore_for_file: no_leading_underscores_for_local_identifiers, file_names
+
 import 'package:flutter/material.dart';
 import 'package:wtdt/components/CustomButtom.dart';
 import 'package:wtdt/components/CustomInputForm.dart';
 import 'package:wtdt/components/CustomText.dart';
 import 'package:wtdt/pages/Homepage.dart';
+import 'package:wtdt/pages/SignUpPage.dart';
 
-class LoginPage extends StatelessWidget{
-
+class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
-  Widget build(BuildContext context){
-
+  Widget build(BuildContext context) {
     final TextEditingController _emailController = TextEditingController();
     final TextEditingController _passwordController = TextEditingController();
 
     return Scaffold(
       backgroundColor: Colors.brown[100],
-      body:  Column(
+      body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
-
         children: [
-
-          const Row(
-              children: [
-                Padding(
-                    padding: EdgeInsets.fromLTRB(72.0, 0.0 ,0.0, 8.0),
-                    child: CustomText(textLabel: "E-mail", size: 20)
-                ),
-              ]
-          ),
-
-           Padding(
+          const Row(children: [
+            Padding(
+                padding: EdgeInsets.fromLTRB(72.0, 0.0, 0.0, 8.0),
+                child: CustomText(textLabel: "E-mail", size: 20)),
+          ]),
+          Padding(
               padding: const EdgeInsets.fromLTRB(64.0, 0.0, 64.0, 32.0),
-              child: CustomInputForm(primaryColor: Colors.black, secondaryColor: Colors.grey, controller: _emailController,)
-          ),
-
+              child: CustomInputForm(
+                primaryColor: Colors.black,
+                secondaryColor: Colors.grey,
+                controller: _emailController,
+              )),
           const Row(
             children: [
               Padding(
-                padding: EdgeInsets.fromLTRB(72.0, 0.0 ,0.0, 8.0),
+                padding: EdgeInsets.fromLTRB(72.0, 0.0, 0.0, 8.0),
                 child: CustomText(textLabel: "Senha", size: 20),
               ),
             ],
           ),
-
-           Padding(
+          Padding(
               padding: const EdgeInsets.fromLTRB(64.0, 0.0, 64.0, 32.0),
-              child: CustomInputForm(primaryColor: Colors.black, secondaryColor: Colors.grey, controller: _passwordController,)
+              child: CustomInputForm(
+                primaryColor: Colors.black,
+                secondaryColor: Colors.grey,
+                controller: _passwordController,
+              )),
+          CustomButtom(
+            textLabel: 'Entrar',
+            primaryColor: Colors.brown,
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                final String email = _emailController.text;
+                final String password = _passwordController.text;
+                // ignore: avoid_print
+                // print("emial: $email, senha: $password");
+                return const HomePage();
+              }));
+            },
           ),
-
-          CustomButtom(textLabel: 'Entrar', primaryColor: Colors.brown, onPressed: () {
-            Navigator.push(context, MaterialPageRoute(
-                builder: (context) {
-                  final String email = _emailController.text;
-                  final String password = _passwordController.text;
-                  // ignore: avoid_print
-                  // print("emial: $email, senha: $password");
-                  return const HomePage();
-                }
-            ));
-          },)
-
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return SignUpPage();
+                  }));
+                },
+                child: const Text(
+                  "Criar conta",
+                  style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.brown,
+                      decoration: TextDecoration.underline
+                    ),
+                )),
+          )
         ],
       ),
     );
-
   }
-
 }
